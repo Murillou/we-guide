e92e278577b6af74b4dd8dc18178dd1e5ab3dafa
Mori <murillovinicius13@hotmail.com>
2024-10-23 06:06:25 -0300

    [feat] estruturando a tela de login

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,6 +1,19 @@
-export function Login(){
-  return(
-    <h1 className="text-center">teste</h1>
-  )
+import logoWeGuide from '../assets/we-guide-extenso.png';
 
+export function Login() {
+  return (
+    <section className="d-flex flex-column align-items-center justify-items-center min-vh-100">
+      <img src={logoWeGuide} alt="" />
+      <form className="d-flex flex-column  ">
+        <p>Faça login</p>
+        <p>Vamos embarcar juntos nessa jornada!</p>
+        <label></label>
+        <input type="e-mail" />
+        <label></label>
+        <input type="password" />
+        <a> Esqueci minha senha </a>
+        <input type="submit" />
+      </form>
+    </section>
+  );
 }
