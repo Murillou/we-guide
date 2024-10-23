d81167d0317f1cf08b219195249846a358eee744
Mori <murillovinicius13@hotmail.com>
2024-10-23 06:29:06 -0300

    [feat] estilizando

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,18 +1,21 @@
 import logoWeGuide from '../assets/we-guide-extenso.png';
+import './Login.css';
 
 export function Login() {
   return (
-    <section className="d-flex flex-column align-items-center justify-items-center min-vh-100">
+    <section className="d-flex flex-column align-items-center justify-content-center min-vh-100 custom-background">
       <img src={logoWeGuide} alt="" />
-      <form className="d-flex flex-column  ">
-        <p>Faça login</p>
-        <p>Vamos embarcar juntos nessa jornada!</p>
+      <form className="d-flex flex-column bg-white py-5 px-3 custom-rounded custom-shadow">
+        <p className="custom-gray-strong-color">Faça login</p>
+        <p className="custom-gray-neutral-color">
+          Vamos embarcar juntos nessa jornada!
+        </p>
         <label></label>
-        <input type="e-mail" />
+        <input type="e-mail" className="form-control" />
         <label></label>
-        <input type="password" />
-        <a> Esqueci minha senha </a>
-        <input type="submit" />
+        <input type="password" className="form-control" />
+        <a href="#"> Esqueci minha senha </a>
+        <input type="submit" value="Entrar" />
       </form>
     </section>
   );
