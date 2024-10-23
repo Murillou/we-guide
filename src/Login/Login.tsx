fc82389f3a94f0e3913e1b6737a82fc504727e69
Mori <murillovinicius13@hotmail.com>
2024-10-23 09:24:23 -0300

    [feat] divisão da imagem com forms na tela de login

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -15,81 +15,77 @@ export function Login() {
   }
 
   return (
-    <section className="d-flex flex-column align-items-center justify-content-center min-vh-100 custom-background">
-      <img
-        src={logoWeGuide}
-        alt="Logo We Guide"
-        className="custom-margin-bottom-logo"
-      />
-      <form className="d-flex flex-column bg-white custom-padding-vertical-forms px-3 custom-rounded custom-shadow">
-        <h6 className="custom-gray-strong-color text-center">Faça login</h6>
-        <p className="custom-gray-neutral-color">
-          Vamos embarcar juntos nessa jornada!
-        </p>
-
-        <div className="d-flex flex-column gap-3">
-          <input
-            type="e-mail"
-            className="custom-input-forms"
-            placeholder="E-mail"
-          />
-
-          <div className="position-relative">
-            <input
-              // Operador ternário para alterar o tipo do input conforme o valor do estado
-              type={showPassword ? 'text' : 'password'}
-              className="custom-input-forms"
-              placeholder="Senha"
-            />
-
-            <i
-              // Operador ternário para alterar o icone conforme o valor do estado
-              className={`fas ${
-                showPassword ? 'fa-eye' : 'fa-eye-slash'
-              } custom-eye-icon`}
-              onClick={togglePasswordVisibility}
-            ></i>
-          </div>
-        </div>
-
-        <a href="#" className="custom-links-text my-3">
-          Esqueci minha senha
-        </a>
-
-        <input
-          type="submit"
-          value="Entrar"
-          className="custom-input-forms custom-submit-input"
+    <div className="login-container">
+      <section className="form-section d-flex flex-column align-items-center justify-content-center min-vh-100 ">
+        <img
+          src={logoWeGuide}
+          alt="Logo We Guide"
+          className="custom-margin-bottom-logo"
         />
-
-        <div className="d-flex align-items-center my-3">
-          <img src={divisor} alt="traço" />
-          <span className="mx-2">ou</span>
-          <img src={divisor} alt="traço" />
-        </div>
-
-        <div className="d-flex aligns-items-center justify-content-center mx-auto gap-3">
-          <a href="#">
-            <img src={googleLogo} alt="" className="custom-social" />
-          </a>
-
-          <a href="#">
-            <img
-              src={facebookLogo}
-              alt=""
-              className="custom-logo-social-facebook custom-social"
+        <form className="d-flex flex-column bg-white custom-padding-vertical-forms px-3 custom-rounded custom-shadow">
+          <h6 className="custom-gray-strong-color text-center">Faça login</h6>
+          <p className="custom-gray-neutral-color">
+            Vamos embarcar juntos nessa jornada!
+          </p>
+          <div className="d-flex flex-column gap-3">
+            <input
+              type="e-mail"
+              className="custom-input-forms"
+              placeholder="E-mail"
             />
+            <div className="position-relative">
+              <input
+                // Operador ternário para alterar o tipo do input conforme o valor do estado
+                type={showPassword ? 'text' : 'password'}
+                className="custom-input-forms"
+                placeholder="Senha"
+              />
+              <i
+                // Operador ternário para alterar o icone conforme o valor do estado
+                className={`fas ${
+                  showPassword ? 'fa-eye' : 'fa-eye-slash'
+                } custom-eye-icon`}
+                onClick={togglePasswordVisibility}
+              ></i>
+            </div>
+          </div>
+          <a href="#" className="custom-links-text my-3">
+            Esqueci minha senha
           </a>
-        </div>
-
-        <div className="mx-auto my-2 ">
-          <span className="custom-pattern-text">Ainda não tem uma conta?</span>{' '}
-          {''}
-          <a href="#" className="custom-links-text">
-            Cadastre-se
-          </a>
-        </div>
-      </form>
-    </section>
+          <input
+            type="submit"
+            value="Entrar"
+            className="custom-input-forms custom-submit-input"
+          />
+          <div className="d-flex align-items-center my-3">
+            <img src={divisor} alt="traço" />
+            <span className="mx-2">ou</span>
+            <img src={divisor} alt="traço" />
+          </div>
+          <div className="d-flex align-items-center justify-content-center mx-auto gap-3">
+            <a href="#">
+              <img src={googleLogo} alt="" className="custom-social" />
+            </a>
+            <a href="#">
+              <img
+                src={facebookLogo}
+                alt=""
+                className="custom-logo-social-facebook custom-social"
+              />
+            </a>
+          </div>
+          <div className="mx-auto my-2 ">
+            <span className="custom-pattern-text">
+              Ainda não tem uma conta?
+            </span>{' '}
+            {''}
+            <a href="#" className="custom-links-text">
+              Cadastre-se
+            </a>
+          </div>
+        </form>
+      </section>
+      <div className="image-section"></div>
+    </div>
   );
 }
